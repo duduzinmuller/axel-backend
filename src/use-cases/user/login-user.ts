2dc00d976b684c82b16ba08ba694d2a11fcd3448
Eduardo <eduardomuller989@gmail.com>
2025-04-15 15:33:03 -0400

    feat: add LoginUserUseCase

---
+++ src/use-cases/user/login-user.ts
@@ -0,0 +1,40 @@
+import { PasswordComparatorAdapter } from "../../adapters/password-comparator";
+import { TokensGeneratorAdapter } from "../../adapters/token-generator";
+import { InvalidPasswordError, UserNotFoundError } from "../../errors/user";
+import { GetUserByEmailRepository } from "../../repositories/user/get-by-email-user";
+
+export class LoginUserUseCase {
+  getUserByEmailRepository: GetUserByEmailRepository;
+  passwordComparatorAdapter: PasswordComparatorAdapter;
+  tokensGeneratorAdapter: TokensGeneratorAdapter;
+  constructor(
+    getUserByEmailRepository: GetUserByEmailRepository,
+    passwordComparatorAdapter: PasswordComparatorAdapter,
+    tokensGeneratorAdapter: TokensGeneratorAdapter,
+  ) {
+    this.getUserByEmailRepository = getUserByEmailRepository;
+    this.passwordComparatorAdapter = passwordComparatorAdapter;
+    this.tokensGeneratorAdapter = tokensGeneratorAdapter;
+  }
+  async execute(email: string, password: string) {
+    const user = await this.getUserByEmailRepository.execute(email);
+
+    if (!user) {
+      throw new UserNotFoundError("Usuario não encontrado");
+    }
+
+    const isPasswordValid = this.passwordComparatorAdapter.execute(
+      password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new InvalidPasswordError();
+    }
+
+    return {
+      ...user,
+      tokens: this.tokensGeneratorAdapter.execute(user.id),
+    };
+  }
+}
