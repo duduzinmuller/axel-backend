6107cb0cad54180fbc0fac4c5926cde2cea4c2e5
Eduardo <eduardomuller989@gmail.com>
2025-04-15 14:26:39 -0400

    feat: add DeleteUserController

---
+++ src/controller/user/delete-user.ts
@@ -0,0 +1,33 @@
+import { UserNotFoundError } from "../../errors/user";
+import { DeleteUserUseCase } from "../../use-cases/user/delete-user";
+import { ok, serverError } from "../helpers/http";
+import { userNotFoundResponse } from "../helpers/user";
+import { checkIfIdIsValid, invalidIdResponse } from "../helpers/validation";
+
+export class DeleteUserController {
+  deleteUserUseCase: DeleteUserUseCase;
+  constructor(deleteUserUseCase: DeleteUserUseCase) {
+    this.deleteUserUseCase = deleteUserUseCase;
+  }
+  async execute(httpRequest: any) {
+    try {
+      const userId = httpRequest.params.userId;
+
+      const idIsValid = checkIfIdIsValid(userId);
+
+      if (!idIsValid) {
+        return invalidIdResponse("Este id e invalido");
+      }
+
+      const result = await this.deleteUserUseCase.execute(userId);
+
+      return ok(result);
+    } catch (error) {
+      if (error instanceof UserNotFoundError) {
+        return userNotFoundResponse("Usuario não encontrado");
+      }
+      console.error(error);
+      return serverError();
+    }
+  }
+}
