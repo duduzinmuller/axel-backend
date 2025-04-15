a45cafc772c63858761a43ea38b86a6c048de991
Eduardo <eduardomuller989@gmail.com>
2025-04-15 02:58:41 -0400

    feat: add GetUserByIdUseCase

---
+++ src/use-cases/user/get-user-by-id.ts
@@ -0,0 +1,13 @@
+import { GetUserByIdRepository } from "../../repositories/user/get-by-id-user";
+
+export class GetUserByIdUseCase {
+  getUserByIdRepository: GetUserByIdRepository;
+  constructor(getUserByIdRepository: GetUserByIdRepository) {
+    this.getUserByIdRepository = getUserByIdRepository;
+  }
+  async execute(userId: string) {
+    const result = await this.getUserByIdRepository.execute(userId);
+
+    return result;
+  }
+}
