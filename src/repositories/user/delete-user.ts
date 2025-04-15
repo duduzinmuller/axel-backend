c2d56902d6f1d1b94acb382080376c67487dcbb8
Eduardo <eduardomuller989@gmail.com>
2025-04-15 14:19:40 -0400

    feat: add DeleteUserRepository

---
+++ src/repositories/user/delete-user.ts
@@ -0,0 +1,24 @@
+import prisma from "../../../prisma/prisma";
+import { UserNotFoundError } from "../../errors/user";
+import { PrismaClientKnownRequestError } from "../../generated/prisma/runtime/library";
+
+export class DeleteUserRepository {
+  async execute(userId: string) {
+    try {
+      return await prisma.user.delete({
+        where: {
+          id: userId,
+        },
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // P2025 = "An operation failed because it depends on one or more records that were required but not found" (from Prisma docs)
+        if (error.code === "P2025") {
+          throw new UserNotFoundError(userId);
+        }
+      }
+
+      throw error;
+    }
+  }
+}
