1a1643f135bbaf161ca843baeed4581cb7ad661b
Eduardo <eduardomuller989@gmail.com>
2025-04-15 03:31:56 -0400

    feat: add UpdateUserRepository

---
+++ src/repositories/user/update-user.ts
@@ -0,0 +1,27 @@
+import prisma from "../../../prisma/prisma";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library.js";
+import { userNotFoundResponse } from "../../controller/helpers/user";
+
+export class UpdateUserRepository {
+  async execute(userId: string, updateUserParams: any) {
+    try {
+      const user = await prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data: updateUserParams,
+      });
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // P2025 = "An operation failed because it depends on one or more records that were required but not found" (from Prisma docs)
+        if (error.code === "P2025") {
+          throw userNotFoundResponse(userId);
+        }
+      }
+
+      throw error;
+    }
+  }
+}
