726e82f0eb76df56cce8f4861009e59316e9918d
Eduardo <eduardomuller989@gmail.com>
2025-04-16 16:35:09 -0400

    feat: add GetOrCreateUserByProviderUseCase

---
+++ src/use-cases/user/get-or-create-user-by-provider.ts
@@ -0,0 +1,31 @@
+import { GetOrCreateUserByProviderRepository } from "../../repositories/user/get-or-create-user-by-provider";
+import { TokensGeneratorAdapter } from "../../adapters/token-generator";
+import { Provider } from "../../generated/prisma";
+
+export class GetOrCreateUserByProviderUseCase {
+  getOrCreateUserByProviderRepository: GetOrCreateUserByProviderRepository;
+  tokensGeneratorAdapter: TokensGeneratorAdapter;
+
+  constructor(
+    getOrCreateUserByProviderRepository: GetOrCreateUserByProviderRepository,
+    tokensGeneratorAdapter: TokensGeneratorAdapter,
+  ) {
+    this.getOrCreateUserByProviderRepository =
+      getOrCreateUserByProviderRepository;
+    this.tokensGeneratorAdapter = tokensGeneratorAdapter;
+  }
+  async execute(provider: Provider, token: string, adapter: any) {
+    const userData = await adapter.verifyToken(token);
+
+    const user = await this.getOrCreateUserByProviderRepository.execute(
+      provider,
+      userData.providerId,
+      userData,
+    );
+
+    return {
+      ...user,
+      tokens: this.tokensGeneratorAdapter.execute(user.id),
+    };
+  }
+}
