5e00a1d0dfc76a6c9e2c60944820bedf3be4cf3d
Eduardo <eduardomuller989@gmail.com>
2025-04-16 21:45:10 -0400

    feat: enhance CreateVerificationUseCase to generate unique verification codes and timestamps

--- src/use-cases/email-verification/email-verification.ts
+++ src/use-cases/email-verification/email-verification.ts
@@ -1,5 +1,7 @@
 import { CreateVerificationRepository } from "../../repositories/email-verification/email-verification";
 import { EmailVerification } from "../../types/user";
+import { generateCode } from "../../utils/code-generator";
+import { v4 as uuidv4 } from "uuid";
 
 export class CreateVerificationUseCase {
   createVerificationRepository: CreateVerificationRepository;
@@ -7,9 +9,21 @@ export class CreateVerificationUseCase {
     this.createVerificationRepository = createVerificationRepository;
   }
   async execute(emailVerificationParams: EmailVerification) {
-    const result = await this.createVerificationRepository.execute(
-      emailVerificationParams,
-    );
+    const code = generateCode();
+    const expiresAt = new Date(Date.now() + 15 * 60 * 1000);
+
+    const emailVerification: EmailVerification = {
+      id: uuidv4(),
+      userId: emailVerificationParams.userId,
+      code,
+      expiresAt,
+      contactId: emailVerificationParams.contactId,
+      createdAt: new Date(),
+    };
+
+    const result =
+      await this.createVerificationRepository.execute(emailVerification);
+
     return result;
   }
 }
