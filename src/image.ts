0a50c90a5471a5cb8e76e23444c399e105ef6021
Eduardo <eduardomuller989@gmail.com>
2025-05-09 14:40:06 -0400

    refactor: add new redirect link profile image

--- src/image.ts
+++ src/image.ts
@@ -1,3 +1,3 @@
 export const imageUrl = (): string => {
-  return "https://sdmntprsouthcentralus.oaiusercontent.com/files/00000000-f8b4-61f7-9860-2d553cd02da1/raw?se=2025-04-14T19%3A54%3A03Z&sp=r&sv=2024-08-04&sr=b&scid=99f02b51-766a-59fe-9596-1e7448c5f781&skoid=aa8389fc-fad7-4f8c-9921-3c583664d512&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-04-14T11%3A48%3A06Z&ske=2025-04-15T11%3A48%3A06Z&sks=b&skv=2024-08-04&sig=71FEu6RnVeViDLKUpn5xSlL34RypnFRzhw1FevyMe7k%3D";
+  return "https://imgur.com/5Q9j39l";
 };
