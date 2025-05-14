de22f7cbf16fbe4d865240017111f032ad8840ba
Eduardo <eduardomuller989@gmail.com>
2025-05-14 14:12:39 -0400

    fix: hash password before saving on user update

--- src/adapters/password-hasher.ts
+++ src/adapters/password-hasher.ts
@@ -1,12 +1,7 @@
 import bcrypt from "bcrypt";
 
 export class PasswordHasherAdapter {
-  async execute(password: string): Promise<string> {
-    if (!password) {
-      throw new Error("Password is required to hash");
-    }
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    return hashedPassword;
+  execute(password: string) {
+    return bcrypt.hash(password, 10);
   }
 }
