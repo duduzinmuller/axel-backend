406b1e31fb574c0d1b5ff25993f0dbc608e78879
Eduardo <eduardomuller989@gmail.com>
2025-06-06 16:44:33 -0400

    refactor: remove unused services, controllers, and routes for social media and camera functionalities

--- src/config/spotify.ts
+++
@@ -1,5 +0,0 @@
-export const spotifyConfig = {
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: process.env.SPOTIFY_REDIRECT_URI,
-};
--- src/config/tv-config.ts
+++
@@ -1,6 +0,0 @@
-export const tvConfig = {
-  subnet: process.env.SUBNET,
-  ssdpSearchTarget: "urn:schemas-upnp-org:device:MediaRenderer:1",
-  tvControlPort: 8008,
-  timeoutMs: 2000,
-};
