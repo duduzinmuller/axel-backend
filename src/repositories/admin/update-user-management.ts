256cf4ccc1f1b267e7bd6371204a4e4d34000e2d
duduzinmuller <eduardomuller989@gmail.com>
2025-07-12 10:55:00 -0400

    refactor: update execute method to accept for improved type safety

--- src/repositories/admin/update-user-management.ts
+++ src/repositories/admin/update-user-management.ts
@@ -1,7 +1,11 @@
+import { Prisma } from "@prisma/client";
 import prisma from "../../../prisma/prisma";
 
 export class UpdateUserManagmentRepository {
-  async execute(userId: string, updateUserParams: any) {
+  async execute(
+    userId: string,
+    updateUserParams: Partial<Prisma.UserUpdateInput>,
+  ) {
     const user = await prisma.user.update({
       where: { id: userId },
       data: updateUserParams,
