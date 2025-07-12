597ac2a54ad5d1b1417322462624a9a97b122294
duduzinmuller <eduardomuller989@gmail.com>
2025-07-12 19:47:27 -0400

    feat: add plan expiration logic to UpdateUserPlanRepository

--- src/repositories/user/update-user-plan.ts
+++ src/repositories/user/update-user-plan.ts
@@ -2,9 +2,25 @@ import prisma from "../../../prisma/prisma";
 
 export class UpdateUserPlanRepository {
   async execute(userId: string, plan: string) {
+    let planExpiresAt = null;
+    const now = new Date();
+
+    if (plan === "MONTHLY") {
+      planExpiresAt = new Date(now.getTime() + 30 * 24 * 60 * 60 * 1000);
+    } else if (plan === "ANNUAL") {
+      planExpiresAt = new Date(
+        now.getFullYear() + 1,
+        now.getMonth(),
+        now.getDate(),
+      );
+    }
+
     return prisma.user.update({
       where: { id: userId },
-      data: { plan: plan as any },
+      data: {
+        plan: plan as any,
+        planExpiresAt,
+      },
     });
   }
 }
